8fd3501aeed2df64817cdc3da859b626b514c97b
Veda Venkatesh Kamat <nnm24ee128@nmamit.in>
2026-06-08 23:23:13 +0530

    Add files via upload

---
+++ NNM24EE128/c_programming_2_V0B.c
@@ -0,0 +1,16 @@
+//Author:Veda Venkatesh Kamat
+//USN: NNM24EE128
+//Date: 05 June 2026
+//Department: Electrical and elctronics
+//College: NMAM Institute of Technology
+//About the program: This program includes a user defines header file, it calls the function from the header file and performs the operation
+//Reference:geeksforgeeks.org
+
+#include "vhead.h" //user defined header file
+int main()
+{
+    add(4, 6); //calls the add function from the header file
+    multiply(5, 5); //calls the multiplication function from the header file
+    printf("My name is VEDA"); //print the text
+    return 0;
+}
