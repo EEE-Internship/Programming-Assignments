14ccf9c4b443af864ccde3f230621d52c35bdc9a
nnm24ee121-sushanth <nnm24ee121@nmamit.in>
2026-06-09 23:00:39 +0530

    To prove curl of H is perpendicular to H vector

---
+++ To_prove_curl_of_H_is_perpendicular_to_H.c
@@ -0,0 +1,31 @@
+/***
+ * Name:Sushanth Shetty
+ * Usn:NNM24EE121
+ * DATE;06-JUNE-2026
+ * DEPT:Electrical and electronics engineering
+ * College:NMAMIT
+ */
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int x,y,dy_Hz=0,dx_Hz=0,dz_Hx=0,dx_Hy=1,dy_Hx=-1,Hxx,Hyy,Hzz,perpendicular,hz;
+    char hx,hy;
+    hx =-y;
+    hy =x;
+    hz=0;
+    printf("The H vector is -y i + x j+ o k \n");
+    Hxx=dy_Hz-dz_Hx;
+    Hyy=dx_Hz-dz_Hx;
+    Hzz=dx_Hy-dy_Hx;
+    printf( "the curl of H vector is %d i+ %d j+ %d k \n",Hxx,Hyy,Hzz);
+    perpendicular=Hxx*hx+Hyy*hy+Hzz*hz;
+    if(perpendicular==0)
+    {
+        printf("The Divergence of curl of H vector is %d .\n",perpendicular);
+    printf("Therefore we can say that  curl of H vector is perpendicular to H vector. \n");
+    printf("Hence proved.");
+    
+    }
+    return(0);
+}
