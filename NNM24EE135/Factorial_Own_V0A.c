ae45fea63e6b6d24702aab77c31cb6d97b0055a6
Vrushabh P Ram <nnm24ee135@nmamit.in>
2026-06-14 15:15:51 +0530

    Update Factorial_Own_V0A.c

--- NNM24EE135/Factorial_Own_V0A.c
+++ NNM24EE135/Factorial_Own_V0A.c
@@ -3,8 +3,7 @@
   Date: 07-June-2026
   Department: Electrical and Electronics Engineering 
   College: NMAM Instutute of Technology
-  About the program: This program prints the values of each variable along with their size 
-                     and location*/
+  About the program: This program print factorial of input number*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "factorial.h"
