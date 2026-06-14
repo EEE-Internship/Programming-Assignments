863ff96b654a93d4dcd7387e9528d6648876f5ca
Anwitha Bhat <nnm24ee022@nmamit.in>
2026-06-14 21:11:40 +0530

    Update 11-June-2026_Rank_V0ABc

--- NNM24EE022/11-June-2026_Rank_V0A.c
+++ NNM24EE022/11-June-2026_Rank_V0A.c
@@ -18,6 +18,35 @@ int main()
     }
 
     rank = n;
-    }
+    
+  for(i=0;i<rank;i++)
+    {
+ if(a[i][i] != 0)
+{
+   for(j=0;j<m;j++)
+      {
+      if(j != i)
+       {
+       factor = (float)a[j][i] / a[i][i];
+
+         for(k=0;k<rank;k++)
+         a[j][k] = a[j][k] - factor*a[i][k];
+            }
+            }
+         }
+        else
+        {
+         rank--;
+
+        for(j=0;j<m;j++)
+         a[j][i] = a[j][rank];
+
+         i--;
+        }
+      }
+
+    printf("Rank = %d", rank);
+ return 0;
+}
 
  
