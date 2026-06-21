3c72dadaca974762c12e4d4c8c35bfc0d7a8da18
nnm24ee035-dg <nnm24ee035@nmamit.in>
2026-06-21 20:12:03 +0530

    Update n cross m.c

--- NNM24EE035/n cross m.c
+++ NNM24EE035/n cross m.c
@@ -1,58 +1,20 @@
+/*AUTHOR=DEEPIKA G
+USN=NNM24EE035
+Date=10 JUNE 2026
+Department=Electrical and Electronics
+College=NMAM Institute of Technology
+To find the inverse of nxm matrix*/
 #include <stdio.h>
-
 int main()
- {
-    int n, i, j, k;
-    float a[10][20], ratio;
-
-    printf("Enter order of matrix: ");
-    scanf("%d", &n);
-
-    printf("Enter matrix elements:\n");
-    for(i = 0; i < n; i++) 
-    {
-        for(j = 0; j < n; j++) 
-        {
-            scanf("%f", &a[i][j]);
-        }
-    }
-    for(i = 0; i < n; i++) 
-    {
-        for(j = n; j < 2 * n; j++) 
-        {
-            a[i][j] = (i == (j - n)) ? 1 : 0;
-        }
-    }
-    for(i = 0; i < n; i++) 
-    {
-        float pivot = a[i][i];
-
-        for(j = 0; j < 2 * n; j++)
-            a[i][j] /= pivot;
-
-        for(k = 0; k < n; k++)
-         {
-            if(k != i)
-             {
-                ratio = a[k][i];
-
-                for(j = 0; j < 2 * n; j++)
-                 {
-                    a[k][j] -= ratio * a[i][j];
-                }
-            }
-        }
-    }
-
-    printf("\nInverse Matrix:\n");
-    for(i = 0; i < n; i++)
-     {
-        for(j = n; j < 2 * n; j++)
-         {
-            printf("%.2f ", a[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+{
+int m,n;
+  printf("Enter the values of row:");
+  scanf("%d,n");
+printf("Enter the values of columns");
+scanf("%d,n");
+if(n!=m)
+   {
+    printf("Inverse matrix exist only for the Square matrix");
+   }
+        return 0;
 }
